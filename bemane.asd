4a64f6a4e2925cd660767a27b095ee24413cde42
David Pflug <david@pflug.io>
2022-01-17 10:26:13 -0500

    Initial commit, working downloads

---
+++ bemane.asd
@@ -0,0 +1,12 @@
+(defsystem bemane
+  :version "0.1"
+  :author "David Pflug"
+  :mailto "david@pflug.io"
+  :homepage "https://pflug.io"
+  :source-control (:git "ssh://git@pflug.io:d/bemane.git")
+  :license "GNU AGPLv3"
+  :defsystem-depends-on ()
+  :depends-on (:dexador :lquery :lparallel :arrows)
+  :pathname "src"
+  :components ((:file "bemane"))
+  :description "Short script to keep a dir of BEMA episodes up-to-date")
---
+++ src/bemane.lisp
@@ -0,0 +1,28 @@
+(in-package :cl-user)
+(defpackage :bemane
+  (:use :cl)
+  (:export :main)
+  (:import-from :arrows :-<>)
+  (:local-nicknames (:d :dexador)
+                    (:l :lquery)
+                    (:p :lparallel)))
+(in-package :bemane)
+
+(setf p:*kernel* nil)
+
+(defun main ()
+  (setf p:*kernel* (p:make-kernel 3))
+  (-<> (d:get "https://www.bemadiscipleship.com/rss")
+       (plump:parse)
+       (clss:select "rss channel item" <>)
+       (download-items)))
+
+(defun download-items (items)
+  (p:pmap 'vector #'downloader items))
+
+(defun downloader (item)
+  (let* ((title (vector-pop (l:$  item "title" (text))))
+         (fn (uiop:ensure-pathname (concatenate 'string title ".mp3")))
+         (url (vector-pop (l:$ item "enclosure" (attr "url")))))
+    (when (not (uiop:file-exists-p fn))
+      (d:fetch url fn))))
